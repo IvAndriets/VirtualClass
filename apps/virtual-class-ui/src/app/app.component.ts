31e9b596769296c176973b36e0f520c466aae105
Olha Andriiets <osandriets@gmail.com>
2023-04-23 17:22:07 +0200

    init

---
+++ apps/virtual-class-ui/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "virtual-class-frontend-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+})
+export class AppComponent {
+  title = "virtual-class-ui";
+}
