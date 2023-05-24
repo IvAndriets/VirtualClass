3db8b00d4e7ed4b8bffa39bb21878c7c62de49bc
Olha Andriiets <osandriets@gmail.com>
2023-05-24 11:48:07 +0200

    show homework for student

--- libs/virtual-class-courses/src/lib/components/homeworks/homeworks-view/homeworks-view.component.ts
+++ libs/virtual-class-courses/src/lib/components/homeworks/homeworks-view/homeworks-view.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Observable } from "rxjs";
+import { Comments, Homework } from "@virtual-class-frontend/virtual-class-web-api-v1";
+import { CommentsService, HomeworksService } from "@virtual-class-frontend/virtual-class-store";
 
 @Component({
   selector: 'virtual-class-frontend-homeworks-view',
   templateUrl: './homeworks-view.component.html',
   styleUrls: ['./homeworks-view.component.scss'],
 })
-export class HomeworksViewComponent {}
+export class HomeworksViewComponent {
+  @Input() lectureId = '';
+
+  homeworks$!: Observable<Homework[]>
+
+  constructor(
+    private readonly homeworksService: HomeworksService,
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.homeworksService.clearCache();
+    this.homeworksService.loadWithQuery({
+      lecture_id: this.lectureId
+    });
+
+    this.homeworks$ = this.homeworksService.entities$;
+  }
+}
--- libs/virtual-class-web-api-v1/src/lib/interfaces/homework.interface.ts
+++ libs/virtual-class-web-api-v1/src/lib/interfaces/homework.interface.ts
@@ -2,20 +2,13 @@ export interface Homework {
   id: string,
   lecture: string,
   mark: number,
-  file: {
-    // "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-    url: string,
-    // "file_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-    file_name: string,
-    // "description": "string",
-    // "owner": "user@example.com",
-    // "created_at": "2023-05-23T19:10:51.202Z",
-    // "updated_at": "2023-05-23T19:10:51.202Z",
-    // "updated_by": "user@example.com"
-  },
   teacher_comment: string,
   owner: string,
   created_at: Date,
   updated_at: Date,
-  updated_by: string
+  updated_by: string,
+  file: {
+    url: string,
+    file_name: string
+  },
 }
